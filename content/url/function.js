df15a9d538249ff79611031d08b57b9de996fb35
jljiu9 <77003319+jljiu9@users.noreply.github.com>
2021-07-07 18:36:15 +0800

    Update function.js

--- content/url/function.js
+++ content/url/function.js
@@ -8,3 +8,36 @@ function setTime20(dosomething){setTimeout(function (){dosomething();},2000);}
 //setTime(1,2,3,6,10,20)
 //
 //
+//捕获异常函数
+function try_catch(dosothing){
+    try{
+        dosothing();
+    }catch{
+        //console.log('出现异常！');
+    }
+}
+function try_catch_if(dosothing,trueif){ //加了if判断条件
+    try{
+        if(trueif()){
+            dosothing();
+        }else{
+            //console.log('if条件不满足')
+        }
+    }catch{
+        //console.log('出现异常！');
+    }
+}
+//要执行的函数
+// function dosothing(){
+//     m = 'jljiu';
+//     m += 00;
+//     console.log(m)
+// }
+//函数调用
+// trycatch(dosothing);
+// try_catch_if(
+//     dosothing,
+//     function xx(){
+//         return 1
+//     }
+// );
